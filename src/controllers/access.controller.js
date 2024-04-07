9772b6e8cadc8cc4ad40824d36962b2ccceaf3a4
longnguyen77c <longnguyen77c@gmail.com>
2024-04-07 23:18:29 +0700

    Section 8: Make Your API Response use class | Course Node.js Backend Architecture

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const AccessService = require("../services/access.service")
+const {OK, CREATED} = require('../core/success.response')
 
 class AccessController {
     signUp = async (req, res, next) => {
@@ -14,8 +15,15 @@ class AccessController {
         // } catch (error) {
         //     next(error)
         // }
-        return res.status(201).json(await AccessService.signUp(req.body))
 
+        // return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'Registered OK',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10,
+            }
+        }).send(res)
     }
 }
 
